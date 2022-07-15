9056ea98cd6759e223beaa293ef9516fbf3faceb
unknown <ferhaoui.20044@gmail.com>
2022-07-15 10:12:07 +0100

    add - complete card -

---
+++ code.js
@@ -0,0 +1,32 @@
+const router = require('express').Router()
+const Trello = require("trello-node-api")("4c3f73efe799ce3be4134c6262af24c8", "97cb553962782fd607ad992fbc4112c713e1d1d5633026413832d9a1f959e10a")
+
+
+router.post("/complete", async(req, res) => {
+    let data = {
+        // name: req.body.name,
+        // desc: 'Card description',
+        // pos: 'bottom',
+        idList: req.body.idList, //REQUIRED
+        // due: new Date("07/12/2022"),
+        dueComplete: true,
+        // idMembers: [],
+        // idLabels: [],
+        // urlSource: 'https://example.com',
+        // fileSource: 'file',
+        // idCardSource: 'CARD_ID',
+        // keepFromSource: 'attachments,checklists,comments,due,labels,members,stickers'
+    };
+    let response;
+    try {
+        response = await Trello.card.update(req.body.idCard, data);
+    } catch (error) {
+        if (error) {
+            console.log('error ', error);
+            res.status(400).json({error: "error"})
+        }
+    }
+    res.status(200).json({ status: "ok" })
+})
+
+module.exports = router
---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require("cors")
+const bodyParser = require('body-parser')
+
+const r = require('./code.js');
+
+const app = express()
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+app.use('/card', r)
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log('Server Running on port ', PORT))
