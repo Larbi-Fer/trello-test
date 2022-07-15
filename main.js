1aeb76ef7f2b2bf57d25235fa3243122ce8bc76c
unknown <ferhaoui.20044@gmail.com>
2022-07-15 10:46:51 +0100

    edit

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ const bodyParser = require('body-parser')
 const r = require('./code.js');
 
 const app = express()
-
+require('dotenv').config();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors({
-    origin: ["https://ai-way.netlify.app/"]
+    origin: ["https://ai-way.netlify.app/", "https://trello.com"]
 }))
 app.use('/card', r)
 
